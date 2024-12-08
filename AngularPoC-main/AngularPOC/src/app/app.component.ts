0ce790abe84ba4e3895e78c7cc895bb4e46a0e2e
Priyanka Pandit <61827165+kcs-priyankapandit@users.noreply.github.com>
2024-12-08 10:33:35 +0530

    add project code

--- AngularPoC-main/AngularPOC/src/app/app.component.ts
+++ AngularPoC-main/AngularPOC/src/app/app.component.ts
@@ -1,10 +1,69 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'AngularPOC';
+  apiKeyForm: FormGroup;
+  submitted = false;
+  displayedColumns: string[] = ['expand', 'title', 'section'];
+  articles: any = [];
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {
+    this.apiKeyForm = this.formBuilder.group({
+      apiKey: ['', [Validators.required, Validators.minLength(16)]],
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.apiKeyForm.invalid) {
+      return;
+    }
+
+    const apiKey = '18G0sleY3Axwg5hxDpCpovXh6BtQAQDA'; //this.apiKeyForm.value.apiKey;
+
+    this.http
+      .post(
+        'http://localhost:50264/api/Article/SubmitArticle?apiKey=' + apiKey,
+        null
+      )
+      .subscribe({
+        next: (response: any) => {
+          console.log('API key validated successfully:', response);
+          this.articles = response.results;
+          console.log(this.articles);
+          this.articles = this.transformArticlesToTree(response.results || []);
+        },
+        error: (error) => {
+          console.error('Error validating API key:', error);
+          alert('Invalid API Key.');
+        },
+      });
+  }
+
+  transformArticlesToTree(articles: any[]): any[] {
+    return articles.map((article) => ({
+      title: article.title,
+      section: article.section,
+      abstract: article.abstract,
+      expanded: false, // Add expanded property for toggle
+      children: article.multimedia
+        ? article.multimedia.map((media: any) => ({
+            title: `Image - ${media.caption || 'No Caption'}`,
+            section: media.format,
+            abstract: `Type: ${media.type}, Dimensions: ${media.width}x${media.height}`,
+            expanded: false,
+          }))
+        : [],
+    }));
+  }
+
+  toggleExpand(node: any) {
+    node.expanded = !node.expanded;
+  }
 }
--- AngularPoC-main/AngularPOC/src/app/app.module.ts
+++ AngularPoC-main/AngularPOC/src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatTableModule,
+    MatCardModule,
+    MatSortModule,
+    MatPaginatorModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
